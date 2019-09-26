81424a8ebc3054c54bdf958c7ff0c9b6bbbdc3ea
Savas Ziplies <sziplies@elipzis.de>
2019-09-26 10:12:48 +0200

    Fixed: Issue with multiple different types (such as datetime) and a cast mismatch

--- src/NovaDependencyContainer.php
+++ src/NovaDependencyContainer.php
@@ -5,6 +5,8 @@ namespace elipZis\NovaDependencyContainer;
 use Laravel\Nova\Fields\Field;
 use Laravel\Nova\Http\Requests\NovaRequest;
 
+/**
+ */
 class NovaDependencyContainer extends Field {
 	/**
 	 * The field's component.
@@ -64,16 +66,10 @@ class NovaDependencyContainer extends Field {
 	public function resolveForDisplay($resource, $attribute = null) {
 		parent::resolveForDisplay($resource, $attribute);
 
-        foreach ($this->meta['dependencies'] as $index => $dependency) {
-            if (array_key_exists('notEmpty', $dependency) && !empty($resource->{$dependency['field']})) {
-                $this->meta['dependencies'][$index]['satisfied'] = true;
-            }
-
-            if (array_key_exists('value', $dependency) && $dependency['value'] == $resource->{$dependency['field']}) {
-                $this->meta['dependencies'][$index]['satisfied'] = true;
-            }
-        }
-    }
+		foreach($this->meta['dependencies'] as $index => $dependency) {
+			$this->meta['dependencies'][$index]['satisfied'] = $this->isSatisfied($dependency, $resource->{$dependency['field']} ?? null) > 0;
+		}
+	}
 
 	/**
 	 * Retrieve values of dependency fields
@@ -83,121 +79,124 @@ class NovaDependencyContainer extends Field {
 	 * @return array|mixed
 	 */
 	protected function resolveAttribute($resource, $attribute) {
-		foreach($this->meta['fields'] as $field) {
-			$field->resolve($resource);
+		if($this->isDependenciesSatisfied($resource)) {
+			foreach($this->meta['fields'] as $field) {
+				$field->resolve($resource);
+			}
 		}
 
 		return [];
 	}
 
-    /**
-     * Fills the attributes of the model within the container if the dependencies for the container are satisfied.
-     *
-     * @param NovaRequest $request
-     * @param string $requestAttribute
-     * @param object $model
-     * @param string $attribute
-     */
-    protected function fillAttributeFromRequest(NovaRequest $request, $requestAttribute, $model, $attribute)
-    {
-        foreach ($this->meta['fields'] as $field) {
-            $field->fill($request, $model);
-        }
-    }
+	/**
+	 * Fills the attributes of the model within the container if the dependencies for the container are satisfied.
+	 *
+	 * @param NovaRequest $request
+	 * @param string      $requestAttribute
+	 * @param object      $model
+	 * @param string      $attribute
+	 */
+	protected function fillAttributeFromRequest(NovaRequest $request, $requestAttribute, $model, $attribute) {
+		foreach($this->meta['fields'] as $field) {
+			$field->fill($request, $model);
+		}
+	}
 
-    /**
-     * Checks whether to add validation rules
-     *
-     * @param NovaRequest $request
-     * @return bool
-     */
-    protected function isDependenciesSatisfiedForValidationRules(NovaRequest $request)
-    {
-        if (!isset($this->meta['dependencies'])
-            || !is_array($this->meta['dependencies'])) {
-            return false;
-        }
+	/**
+	 * @param      $dependency
+	 * @param null $field
+	 * @return int Zero if not satisfied, otherwise the count of "satisfaction"
+	 */
+	protected function isSatisfied($dependency, $field = null) {
+		$retVal = 0;
+		if(array_key_exists('notEmpty', $dependency) && $field) {
+			$retVal++;
+		}
+		if(array_key_exists('value', $dependency) && $dependency['value'] == $field) {
+			$retVal++;
+		}
+		return $retVal;
+	}
 
-        $satisfiedCounts = 0;
-        foreach ($this->meta['dependencies'] as $index => $dependency) {
-            if (array_key_exists('notEmpty', $dependency) && !empty($request->has($dependency['field']))) {
-                $satisfiedCounts++;
-            }
+	/**
+	 * @return bool
+	 */
+	protected function isDependenciesSatisfied($resource) {
+		if(!isset($this->meta['dependencies'])
+			|| !is_array($this->meta['dependencies'])) {
+			return false;
+		}
 
-            if (array_key_exists('value', $dependency) && $dependency['value'] == $request->get($dependency['field'])) {
-                $satisfiedCounts++;
-            }
-        }
+		$satisfiedCounts = 0;
+		foreach($this->meta['dependencies'] as $index => $dependency) {
+			$satisfiedCounts += $this->isSatisfied($dependency, $resource->{$dependency['field']} ?? $resource->get($dependency['field']) ?? null);
+		}
 
-        return $satisfiedCounts == count($this->meta['dependencies']);
-    }
+		return $satisfiedCounts == count($this->meta['dependencies']);
+	}
 
-    /**
-     * Get a rule set based on field property name
-     *
-     * @param NovaRequest $request
-     * @param string $propertyName
-     * @return array
-     */
-    protected function getSituationalRulesSet(NovaRequest $request, string $propertyName = 'rules')
-    {
-        $fieldsRules = [];
-        if (!$this->isDependenciesSatisfiedForValidationRules($request)
-            || !isset($this->meta['fields'])
-            || !is_array($this->meta['fields'])) {
-            return $fieldsRules;
-        }
+	/**
+	 * Get a rule set based on field property name
+	 *
+	 * @param NovaRequest $request
+	 * @param string      $propertyName
+	 * @return array
+	 */
+	protected function getSituationalRulesSet(NovaRequest $request, string $propertyName = 'rules') {
+		$fieldsRules = [];
+		if(!$this->isDependenciesSatisfied($request)
+			|| !isset($this->meta['fields'])
+			|| !is_array($this->meta['fields'])) {
+			return $fieldsRules;
+		}
 
-        /** @var Field $field */
-        foreach ($this->meta['fields'] as $field) {
-            $fieldsRules[$field->attribute] = is_callable($field->{$propertyName})
-                ? call_user_func($field->{$propertyName}, $request)
-                : $field->{$propertyName};
-        }
+		/** @var Field $field */
+		foreach($this->meta['fields'] as $field) {
+			$fieldsRules[$field->attribute] = is_callable($field->{$propertyName})
+				? call_user_func($field->{$propertyName}, $request)
+				: $field->{$propertyName};
+		}
 
-        return $fieldsRules;
-    }
+		return $fieldsRules;
+	}
 
-    /**
-     * Get the validation rules for this field.
-     *
-     * @param NovaRequest $request
-     * @return array
-     */
-    public function getRules(NovaRequest $request)
-    {
-        return $this->getSituationalRulesSet($request);
-    }
+	/**
+	 * Get the validation rules for this field.
+	 *
+	 * @param NovaRequest $request
+	 * @return array
+	 */
+	public function getRules(NovaRequest $request) {
+		return $this->getSituationalRulesSet($request);
+	}
 
-    /**
-     * Get the creation rules for this field.
-     *
-     * @param NovaRequest $request
-     * @return array|string
-     */
-    public function getCreationRules(NovaRequest $request)
-    {
-        $fieldsRules = $this->getSituationalRulesSet($request, 'creationRules');
+	/**
+	 * Get the creation rules for this field.
+	 *
+	 * @param NovaRequest $request
+	 * @return array|string
+	 */
+	public function getCreationRules(NovaRequest $request) {
+		$fieldsRules = $this->getSituationalRulesSet($request, 'creationRules');
 
-        return array_merge_recursive(
-            $this->getRules($request),
-            $fieldsRules
-        );
-    }
+		return array_merge_recursive(
+			$this->getRules($request),
+			$fieldsRules
+		);
+	}
 
-    /**
-     * Get the update rules for this field.
-     *
-     * @param NovaRequest $request
-     * @return array
-     */
-    public function getUpdateRules(NovaRequest $request)
-    {
-        $fieldsRules = $this->getSituationalRulesSet($request, 'updateRules');
+	/**
+	 * Get the update rules for this field.
+	 *
+	 * @param NovaRequest $request
+	 * @return array
+	 */
+	public function getUpdateRules(NovaRequest $request) {
+		$fieldsRules = $this->getSituationalRulesSet($request, 'updateRules');
 
-        return array_merge_recursive(
-            $this->getRules($request),
-            $fieldsRules
-        );
-    }
+		return array_merge_recursive(
+			$this->getRules($request),
+			$fieldsRules
+		);
+	}
 }
